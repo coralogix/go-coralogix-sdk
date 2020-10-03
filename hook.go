a02d6991d2871dbcaa7c45f00df529e20eeeb7e3
Goldberg <rgoldberg@c02y90a2jgh5.agoda.local>
2020-10-03 12:42:50 +0700

    Adding support for extracting error and handling global report caller

--- hook.go
+++ hook.go
@@ -1,28 +1,60 @@
 package coralogix
 
 import (
+    "fmt"
     "github.com/sirupsen/logrus"
+    "path/filepath"
+    "runtime"
+    "strings"
 )
 
 // Hook is presenting Coralogix Logger for Logrus library
 type Hook struct {
     Writer LoggerManager
+    Options HookOptions
+}
+
+
+type HookOptions struct {
+    // Set to true to extract error message from error type
+    ExtractError bool
+
+    // Set to true to use the global ReportCaller value
+    UseReportCaller bool
 }
 
 // NewCoralogixHook build Coralogix logger hook
-func NewCoralogixHook(PrivateKey string, ApplicationName string, SubsystemName string) *Hook {
+func NewCoralogixHook(PrivateKey string, ApplicationName string, SubsystemName string, options ...HookOptions) *Hook {
+    var op HookOptions;
+    if options != nil && len(options) > 0{
+        op = options[0]
+    }
+
     NewHookInstance := &Hook{
-        *NewLoggerManager(
+        Writer: *NewLoggerManager(
             PrivateKey,
             ApplicationName,
             SubsystemName,
             true,
         ),
+        Options: op,
     }
 
     go NewHookInstance.Writer.Run()
 
     return NewHookInstance
+}
+
+// Extract error message
+func extractError(entry *logrus.Entry) {
+    if entry != nil && entry.Data != nil{
+        for key, val := range entry.Data {
+            e:=val.(error)
+            if e != nil {
+                entry.Data[key] = e.Error()
+            }
+        }
+    }
 }
 
 // Fire send message to Coralogix
@@ -53,6 +85,11 @@ func (hook *Hook) Fire(entry *logrus.Entry) error {
         Level = 6
     }
 
+    // If user is using the global log.SetReportCaller then lets get the data
+    if hook.Options.UseReportCaller && entry.Caller != nil {
+        Category, ClassName, MethodName = getCallerInformation(entry.Caller)
+    }
+
     if Value, Exist := entry.Data["Category"]; Exist {
         Category = Value.(string)
         delete(entry.Data, "Category")
@@ -79,6 +116,10 @@ func (hook *Hook) Fire(entry *logrus.Entry) error {
         delete(entry.Data, "ThreadId")
     } else {
         ThreadId = ""
+    }
+
+    if hook.Options.ExtractError {
+        extractError(entry)
     }
 
     if len(entry.Data) > 0 {
@@ -108,6 +149,20 @@ func (hook *Hook) Fire(entry *logrus.Entry) error {
     }
 
     return nil
+}
+
+func getCallerInformation(frame *runtime.Frame) (string, string, string) {
+    if frame == nil { return "", "", "" }
+    file := filepath.Base(frame.File)
+    sections := strings.Split(frame.Func.Name(), "%")
+    len := len(sections)
+    if len <= 1 {
+        return file, "", frame.Func.Name()
+    }
+    class :=  fmt.Sprintf("%s",sections[len-2:len-1])
+    function :=  fmt.Sprintf("%s",sections[len-1:len])
+
+    return file, class, function
 }
 
 // Levels return levels which can be sent with this hook
