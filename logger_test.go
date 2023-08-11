bef94eaf6c1c2e86b1a8c804aba6b2674899635e
Shaun Remekie <shaun.remekie@coralogix.com>
2023-08-11 18:58:54 +0200

    updated tests

--- logger_test.go
+++ logger_test.go
@@ -1,23 +1,62 @@
 package coralogix
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"reflect"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+const (
+	testPrivateKey = "7569303a-6269-4d2c-bf14-1aec9b1786a4"
+)
+
+// testRequestChan = a channel used to pass requests
+// that have been sent to the mockHTTPServer
+var testRequestChan chan *Bulk
+
+// mockHTTPServer - stores all requests received in a map
+// for validation
+func mockHTTPServer(w http.ResponseWriter, r *http.Request) {
+	var bulk Bulk
+	if err := json.NewDecoder(r.Body).Decode(&bulk); err != nil {
+		log.Fatalf("unable to unmarshal test message: %s", err)
+	}
+
+	testRequestChan <- &bulk
+	defer r.Body.Close()
+	sc := http.StatusOK
+	w.WriteHeader(sc)
+}
+
+func TestMain(m *testing.M) {
+	server := httptest.NewServer(http.HandlerFunc(mockHTTPServer))
+	testRequestChan = make(chan *Bulk, 1)
+	LogURL = server.URL
+	code := m.Run()
+	server.Close()
+	os.Exit(code)
+}
 
 func TestNewLogger(t *testing.T) {
 	NewLoggerTestInstance := NewCoralogixLogger(
 		GetEnv(
 			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
+			testPrivateKey,
 		),
 		"sdk-go",
 		"test",
 	)
 	defer NewLoggerTestInstance.Destroy()
-	if reflect.TypeOf(NewLoggerTestInstance) != reflect.TypeOf(&CoralogixLogger{}) {
-		t.Error("CoralogixLogger creation failed!")
-	}
+	assert.Equal(t,
+		reflect.TypeOf(NewLoggerTestInstance),
+		reflect.TypeOf(&CoralogixLogger{}))
 }
 
 func TestNewLoggerWithoutPrivateKey(t *testing.T) {
@@ -27,39 +66,33 @@ func TestNewLoggerWithoutPrivateKey(t *testing.T) {
 		"test",
 	)
 	defer NewLoggerTestInstance.Destroy()
-	if NewLoggerTestInstance.LoggerManager.PrivateKey != FailedPrivateKey {
-		t.Error("Invalid default value for incorrect private key!")
-	}
+	assert.Equal(t, FailedPrivateKey, NewLoggerTestInstance.LoggerManager.PrivateKey)
 }
 
 func TestNewLoggerWithoutApplicationName(t *testing.T) {
 	NewLoggerTestInstance := NewCoralogixLogger(
 		GetEnv(
 			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
+			testPrivateKey,
 		),
 		"",
 		"test",
 	)
 	defer NewLoggerTestInstance.Destroy()
-	if NewLoggerTestInstance.LoggerManager.ApplicationName != NoAppName {
-		t.Error("Invalid default value for empty application name!")
-	}
+	assert.Equal(t, NoAppName, NewLoggerTestInstance.LoggerManager.ApplicationName)
 }
 
 func TestNewLoggerWithoutSubsystemName(t *testing.T) {
 	NewLoggerTestInstance := NewCoralogixLogger(
 		GetEnv(
 			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
+			testPrivateKey,
 		),
 		"sdk-go",
 		"",
 	)
 	defer NewLoggerTestInstance.Destroy()
-	if NewLoggerTestInstance.LoggerManager.SubsystemName != NoSubSystem {
-		t.Error("Invalid default value for empty subsystem name!")
-	}
+	assert.Equal(t, NoSubSystem, NewLoggerTestInstance.LoggerManager.SubsystemName)
 }
 
 func TestNewLoggerWithCategory(t *testing.T) {
@@ -67,39 +100,35 @@ func TestNewLoggerWithCategory(t *testing.T) {
 	NewLoggerTestInstance := NewCoralogixLoggerWithCategory(
 		GetEnv(
 			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
+			testPrivateKey,
 		),
 		"sdk-go",
 		"test",
 		TestCategory,
 	)
 	defer NewLoggerTestInstance.Destroy()
-	if NewLoggerTestInstance.Category != TestCategory {
-		t.Error("Invalid logger category!")
-	}
+	assert.Equal(t, TestCategory, NewLoggerTestInstance.Category)
 }
 
 func TestNewLoggerWithEmptyCategory(t *testing.T) {
 	NewLoggerTestInstance := NewCoralogixLoggerWithCategory(
 		GetEnv(
 			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
+			testPrivateKey,
 		),
 		"sdk-go",
 		"test",
 		"",
 	)
 	defer NewLoggerTestInstance.Destroy()
-	if NewLoggerTestInstance.Category != LogCategory {
-		t.Error("Category checking is not working!")
-	}
+	assert.Equal(t, LogCategory, NewLoggerTestInstance.Category)
 }
 
 func TestLogger_Destroy(t *testing.T) {
 	NewLoggerTestInstance := NewCoralogixLogger(
 		GetEnv(
 			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
+			testPrivateKey,
 		),
 		"sdk-go",
 		"test",
@@ -108,16 +137,16 @@ func TestLogger_Destroy(t *testing.T) {
 }
 
 func TestLogger_Log(t *testing.T) {
-	NewLoggerTestInstance := NewCoralogixLogger(
+	clogger := NewCoralogixLogger(
 		GetEnv(
 			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
+			testPrivateKey,
 		),
 		"sdk-go",
 		"test",
 	)
-	defer NewLoggerTestInstance.Destroy()
-	NewLoggerTestInstance.Log(
+	defer clogger.Destroy()
+	clogger.Log(
 		Level.DEBUG,
 		"Test message",
 		LogCategory,
@@ -125,115 +154,104 @@ func TestLogger_Log(t *testing.T) {
 		"",
 		"",
 	)
-	if NewLoggerTestInstance.LoggerManager.LogsBuffer.Len() < 1 {
-		t.Error("New log message add failed!")
+
+	clogger.LoggerManager.Flush()
+	bulk := <-testRequestChan
+
+	var msg bool
+	for _, entry := range bulk.LogEntries {
+		msg = entry.Text == "Test message"
 	}
+	assert.True(t, msg, "no matching message found")
 }
 
-func TestLogger_Debug(t *testing.T) {
-	NewLoggerTestInstance := NewCoralogixLogger(
+func TestLoggerSeverity(t *testing.T) {
+	var cxlogger = NewCoralogixLogger(
 		GetEnv(
 			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
+			testPrivateKey,
 		),
 		"sdk-go",
 		"test",
 	)
-	defer NewLoggerTestInstance.Destroy()
-	NewLoggerTestInstance.LoggerManager.Stop()
-	NewLoggerTestInstance.Debug("Test debug message")
-	if NewLoggerTestInstance.LoggerManager.LogsBuffer.Len() < 1 ||
-		NewLoggerTestInstance.LoggerManager.LogsBuffer.Slice(1)[0].Severity != Level.DEBUG {
-		t.Error("Debug log message add failed!")
-	}
-}
 
-func TestLogger_Verbose(t *testing.T) {
-	NewLoggerTestInstance := NewCoralogixLogger(
-		GetEnv(
-			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
-		),
-		"sdk-go",
-		"test",
-	)
-	defer NewLoggerTestInstance.Destroy()
-	NewLoggerTestInstance.LoggerManager.Stop()
-	NewLoggerTestInstance.Verbose("Test verbose message")
-	if NewLoggerTestInstance.LoggerManager.LogsBuffer.Len() < 1 ||
-		NewLoggerTestInstance.LoggerManager.LogsBuffer.Slice(1)[0].Severity != Level.VERBOSE {
-		t.Error("Verbose log message add failed!")
-	}
-}
+	defer cxlogger.Destroy()
 
-func TestLogger_Info(t *testing.T) {
-	NewLoggerTestInstance := NewCoralogixLogger(
-		GetEnv(
-			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
-		),
-		"sdk-go",
-		"test",
-	)
-	defer NewLoggerTestInstance.Destroy()
-	NewLoggerTestInstance.LoggerManager.Stop()
-	NewLoggerTestInstance.Info("Test info message")
-	if NewLoggerTestInstance.LoggerManager.LogsBuffer.Len() < 1 ||
-		NewLoggerTestInstance.LoggerManager.LogsBuffer.Slice(1)[0].Severity != Level.INFO {
-		t.Error("Info log message add failed!")
+	testcases := []struct {
+		name     string
+		clogger  *CoralogixLogger
+		match    string
+		logfn    func(*CoralogixLogger)
+		severity uint
+	}{
+		{
+			name:  "debug",
+			match: "debug",
+			logfn: func(logger *CoralogixLogger) {
+				logger.Debug("Test message DEBUG")
+			},
+			severity: Level.DEBUG,
+		},
+		{
+			name:  "verbose",
+			match: "verbose",
+			// clogger: newtestCoralogixLoggerFn(),
+			logfn: func(logger *CoralogixLogger) {
+				logger.Verbose("Test message VERBOSE")
+			},
+			severity: Level.VERBOSE,
+		},
+		{
+			name: "info",
+			// clogger: newtestCoralogixLoggerFn(),
+			match: "info",
+			logfn: func(logger *CoralogixLogger) {
+				logger.Info("Test message INFO")
+			},
+			severity: Level.INFO,
+		},
+		{
+			name:  "warning",
+			match: "warning",
+			logfn: func(logger *CoralogixLogger) {
+				logger.Warning("Test message WARNING")
+			},
+			severity: Level.WARNING,
+		},
+		{
+			name:  "error",
+			match: "error",
+			logfn: func(logger *CoralogixLogger) {
+				logger.Error("Test message ERROR")
+			},
+			severity: Level.ERROR,
+		},
+		{
+			name:  "critical",
+			match: "critical",
+			// clogger: newtestCoralogixLoggerFn(),
+			logfn: func(logger *CoralogixLogger) {
+				logger.Critical("Test message CRITICAL")
+			},
+			severity: Level.CRITICAL,
+		},
 	}
-}
 
-func TestLogger_Warning(t *testing.T) {
-	NewLoggerTestInstance := NewCoralogixLogger(
-		GetEnv(
-			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
-		),
-		"sdk-go",
-		"test",
-	)
-	defer NewLoggerTestInstance.Destroy()
-	NewLoggerTestInstance.LoggerManager.Stop()
-	NewLoggerTestInstance.Warning("Test warning message")
-	if NewLoggerTestInstance.LoggerManager.LogsBuffer.Len() < 1 ||
-		NewLoggerTestInstance.LoggerManager.LogsBuffer.Slice(1)[0].Severity != Level.WARNING {
-		t.Error("Warning log message add failed!")
-	}
-}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.logfn(cxlogger)
+			cxlogger.LoggerManager.Flush()
+			bulkReq := <-testRequestChan
 
-func TestLogger_Error(t *testing.T) {
-	NewLoggerTestInstance := NewCoralogixLogger(
-		GetEnv(
-			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
-		),
-		"sdk-go",
-		"test",
-	)
-	defer NewLoggerTestInstance.Destroy()
-	NewLoggerTestInstance.LoggerManager.Stop()
-	NewLoggerTestInstance.Error("Test error message")
-	if NewLoggerTestInstance.LoggerManager.LogsBuffer.Len() < 1 ||
-		NewLoggerTestInstance.LoggerManager.LogsBuffer.Slice(1)[0].Severity != Level.ERROR {
-		t.Error("Error log message add failed!")
-	}
-}
+			var msg = true
+			for _, entry := range bulkReq.LogEntries {
+				// fmt.Println(i, entry.Text)
+				if msg = strings.Contains(strings.ToLower(entry.Text), tc.name); msg {
+					assert.Equal(t, tc.severity, entry.Severity)
+				}
+			}
 
-func TestLogger_Critical(t *testing.T) {
-	NewLoggerTestInstance := NewCoralogixLogger(
-		GetEnv(
-			"PRIVATE_KEY",
-			"7569303a-6269-4d2c-bf14-1aec9b1786a4",
-		),
-		"sdk-go",
-		"test",
-	)
-	defer NewLoggerTestInstance.Destroy()
-	NewLoggerTestInstance.LoggerManager.Stop()
-	NewLoggerTestInstance.Critical("Test critical message")
-	if NewLoggerTestInstance.LoggerManager.LogsBuffer.Len() < 1 ||
-		NewLoggerTestInstance.LoggerManager.LogsBuffer.Slice(1)[0].Severity != Level.CRITICAL {
-		t.Error("Critical log message add failed!")
+			assert.True(t, msg, "no matching message found")
+		})
 	}
 }
