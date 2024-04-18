5609d41967be75955091c0eac61a34025247df45
Dima Golomozy <dima.golomozy@gmail.com>
2024-04-18 22:52:40 +0300

    signed (#19)
    Signed-off-by: dimagolomozy <dima.golomozy@gmail.com>
    
    add support for slog WithAttrs
    
    add category, classname and thread id
    
    remove duplicate level
    
    remove next logger and add handler options
    
    Signed-off-by: dimagolomozy <dima.golomozy@gmail.com>
    
    signed
    
    test attrs to map
    
    Signed-off-by: dimagolomozy <dima.golomozy@gmail.com>
    
    fix testss
    
    Signed-off-by: dimagolomozy <dima.golomozy@gmail.com>
    
    signed
    
    fixed tests
    
    implement group
    
    make pointer

--- slog_handler.go
+++ slog_handler.go
@@ -7,11 +7,11 @@ import (
 )
 
 type CoralogixHandler struct {
-	// Next represents the next handler in the chain.
-	Next slog.Handler
-	// cxLogger is the Coralogix logger.
-	cxLogger  *CoralogixLogger
-	AddSource bool
+	cxLogger *CoralogixLogger
+
+	opts   slog.HandlerOptions
+	data   map[string]interface{}
+	groups []string
 }
 
 type source struct {
@@ -22,12 +22,11 @@ type source struct {
 
 type logMessage struct {
 	Message string         `json:"message"`
-	Level   string         `json:"level"`
 	Data    map[string]any `json:"data,omitempty"`
-	Source  source         `json:"source,omitempty"`
+	Source  *source        `json:"source,omitempty"`
 }
 
-func NewCoralogixHandler(privateKey, applicationName, subsystemName string, next slog.Handler) *CoralogixHandler {
+func NewCoralogixHandler(privateKey, applicationName, subsystemName string, opts *slog.HandlerOptions) *CoralogixHandler {
 	logger := NewCoralogixLogger(
 		privateKey,
 		applicationName,
@@ -35,9 +34,27 @@ func NewCoralogixHandler(privateKey, applicationName, subsystemName string, next
 	)
 
 	return &CoralogixHandler{
-		Next:     next,
 		cxLogger: logger,
+		opts:     *opts,
 	}
+}
+
+func (h *CoralogixHandler) cloneData() map[string]interface{} {
+	clone := map[string]interface{}{}
+	for k, v := range h.data {
+		clone[k] = v
+	}
+
+	return clone
+}
+
+func (h *CoralogixHandler) cloneGroups() []string {
+	clone := make([]string, len(h.groups))
+	for i, group := range h.groups {
+		clone[i] = group
+	}
+
+	return clone
 }
 
 // Handle handles the provided log record.
@@ -47,52 +64,83 @@ func (h *CoralogixHandler) Handle(ctx context.Context, r slog.Record) error {
 
 	log := logMessage{
 		Message: r.Message,
-		Level:   r.Level.String(),
-		Data:    map[string]interface{}{},
+		Data:    h.cloneData(),
 	}
 
-	if h.AddSource {
-		log.Source = source{
+	grouped := groupToMap(log.Data, h.groups)
+	if r.NumAttrs() > 0 {
+		r.Attrs(func(a slog.Attr) bool {
+			attrToMap(grouped, a)
+			return true
+		})
+	}
+
+	if h.opts.AddSource {
+		log.Source = &source{
 			Function: f.Function,
 			File:     f.File,
 			Line:     f.Line,
 		}
 	}
 
-	if r.NumAttrs() > 0 {
-		r.Attrs(func(a slog.Attr) bool {
-			attrToMap(log.Data, a)
-			return true
-		})
+	category := ""
+	if v, ok := log.Data["Category"]; ok {
+		category = v.(string)
+		delete(log.Data, "Category")
 	}
 
-	h.cxLogger.Log(levelSlogToCoralogix(r.Level), log, "", "", f.Function, "")
+	className := ""
+	if v, ok := log.Data["ClassName"]; ok {
+		className = v.(string)
+		delete(log.Data, "ClassName")
+	}
 
-	return h.Next.Handle(ctx, r)
+	threadId := ""
+	if v, ok := log.Data["ThreadId"]; ok {
+		threadId = v.(string)
+		delete(log.Data, "ThreadId")
+	}
+
+	h.cxLogger.Log(levelSlogToCoralogix(r.Level), log, category, className, f.Function, threadId)
+	return nil
 }
 
 // WithAttrs returns a new Coralogix whose attributes consists of handler's attributes followed by attrs.
 func (h *CoralogixHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	data := h.cloneData()
+	grouped := groupToMap(data, h.groups)
+	for _, attr := range attrs {
+		attrToMap(grouped, attr)
+	}
+
 	return &CoralogixHandler{
-		Next:      h.Next.WithAttrs(attrs),
-		cxLogger:  h.cxLogger,
-		AddSource: h.AddSource,
+		cxLogger: h.cxLogger,
+		opts:     h.opts,
+
+		data:   data,
+		groups: h.cloneGroups(),
 	}
 }
 
 // WithGroup returns a new Coralogix with a group, provided the group's name.
 func (h *CoralogixHandler) WithGroup(name string) slog.Handler {
 	return &CoralogixHandler{
-		Next:      h.Next.WithGroup(name),
-		cxLogger:  h.cxLogger,
-		AddSource: h.AddSource,
+		cxLogger: h.cxLogger,
+		opts:     h.opts,
+
+		data:   h.cloneData(),
+		groups: append(h.cloneGroups(), name),
 	}
 }
 
 // Enabled reports whether the logger emits log records at the given context and level.
 // Note: We handover the decision down to the next handler.
 func (h *CoralogixHandler) Enabled(ctx context.Context, level slog.Level) bool {
-	return h.Next.Enabled(ctx, level)
+	minLevel := slog.LevelInfo
+	if h.opts.Level != nil {
+		minLevel = h.opts.Level.Level()
+	}
+	return level >= minLevel
 }
 
 func (h *CoralogixHandler) Stop() {
@@ -101,8 +149,6 @@ func (h *CoralogixHandler) Stop() {
 
 func attrToMap(m map[string]any, a slog.Attr) {
 	switch v := a.Value.Any().(type) {
-	case error:
-		m[a.Key] = v.Error()
 	case []slog.Attr:
 		m2 := map[string]any{}
 		for _, a2 := range v {
@@ -110,8 +156,18 @@ func attrToMap(m map[string]any, a slog.Attr) {
 			m[a.Key] = m2
 		}
 	default:
-		m[a.Key] = a.Value.Any()
+		m[a.Key] = v
 	}
+}
+
+func groupToMap(m map[string]any, groups []string) map[string]any {
+	for _, group := range groups {
+		if _, ok := m[group]; !ok {
+			m[group] = map[string]any{}
+		}
+		m = m[group].(map[string]any)
+	}
+	return m
 }
 
 func levelSlogToCoralogix(level slog.Level) uint {
--- slog_handler_test.go
+++ slog_handler_test.go
@@ -3,13 +3,60 @@ package coralogix
 import (
 	"fmt"
 	"log/slog"
-	"os"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestSlogHandler_WithAttrs(t *testing.T) {
+	onTest1 := &CoralogixHandler{}
+	assert.Empty(t, onTest1.data)
+
+	onTest2 := onTest1.WithAttrs([]slog.Attr{
+		slog.String("1", "1"),
+		slog.Int("2", 2),
+	})
+
+	assert.Equal(t, map[string]interface{}{
+		"1": "1",
+		"2": int64(2),
+	}, onTest2.(*CoralogixHandler).data)
+}
+
+func TestSlogHandler_AttrToMap(t *testing.T) {
+	m1 := map[string]any{}
+	attrToMap(m1, slog.String("1", "1"))
+	attrToMap(m1, slog.Int("2", 2))
+
+	assert.Equal(t, map[string]any{
+		"1": "1",
+		"2": int64(2),
+	}, m1)
+
+	m2 := map[string]any{}
+	attrToMap(m2, slog.String("1", "1"))
+	attrToMap(m2, slog.Any("arr", []string{"1", "2"}))
+	assert.Equal(t, map[string]any{
+		"1":   "1",
+		"arr": []string{"1", "2"},
+	}, m2)
+
+	m3 := map[string]any{}
+	attrToMap(m3, slog.String("1", "1"))
+	attrToMap(m3, slog.Group("group",
+		slog.String("2", "2"),
+		slog.Any("arr", []string{"1", "2"}),
+	))
+	assert.Equal(t, map[string]any{
+		"1": "1",
+		"group": map[string]any{
+			"2":   "2",
+			"arr": []string{"1", "2"},
+		},
+	}, m3)
+}
 
 func TestSlogHandler_Send(t *testing.T) {
 	coralogixHandler := NewCoralogixHandler(
@@ -19,9 +66,9 @@ func TestSlogHandler_Send(t *testing.T) {
 		),
 		"sdk-go",
 		"test",
-		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		&slog.HandlerOptions{
 			Level: slog.LevelDebug,
-		}),
+		},
 	)
 	defer func() { recover() }()
 	defer coralogixHandler.Stop()
@@ -70,7 +117,7 @@ func TestSlogHandler_Send(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			msg := fmt.Sprintf("%s (%s)", tc.name, t.Name())
 			tc.logfn(msg, attr)
-			time.Sleep(time.Duration(1) * time.Second)
+			time.Sleep(1 * time.Second)
 			bulk, ok := mockHTTPServerMap[t.Name()]
 			assert.True(t, ok, "%s key not found in mockHTTPServerMap", t.Name())
 
